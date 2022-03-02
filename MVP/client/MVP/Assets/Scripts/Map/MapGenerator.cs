8940dfe19ff9a9b9e83d068dd6789b94f692b43d
Eckaos <nathan.pollart@hotmail.fr>
2022-03-02 13:29:15 -0500

    #4 Déplacement implémenté

--- MVP/client/MVP/Assets/Scripts/Map/MapGenerator.cs
+++ MVP/client/MVP/Assets/Scripts/Map/MapGenerator.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,6 +10,12 @@ public class MapGenerator : MonoBehaviour
     public int xSize = 5;
     public int zSize = 5;
     private int numberOfWalls;
+
+    private GameObject[,] tiles;
+
+    public const string GROUND_TAG = "Ground";
+    public const string WALL_TAG = "Wall";
+    public const string MAP_TAG = "Map";
 
     void Start()
     {
@@ -26,7 +31,7 @@ public class MapGenerator : MonoBehaviour
 
     public void DeleteMap()
     {
-        foreach (GameObject map in GameObject.FindGameObjectsWithTag("Map"))
+        foreach (GameObject map in GameObject.FindGameObjectsWithTag(MAP_TAG))
         {
             DestroyImmediate(map);
         }
@@ -69,8 +74,9 @@ public class MapGenerator : MonoBehaviour
 
     void GenerateShape()
     {
-        GameObject map = new GameObject("Map");
-        map.tag = "Map";
+        GameObject map = new GameObject(MAP_TAG);
+        map.tag = MAP_TAG;
+        tiles = new GameObject[xSize,zSize];
         for (int x = 0; x < xSize; x++)
         {
             GameObject row = new GameObject("Row"+x);
@@ -95,25 +101,187 @@ public class MapGenerator : MonoBehaviour
         if(tileMaterial == null) return null;
         Renderer tileRenderer = tile.GetComponent<Renderer>();
         Material material = new Material(tileMaterial);
-        if(tile.tag == "Wall") material.color = Color.black;
-        if(tile.tag == "Ground") material.color = Color.white;
+        if(tile.tag == WALL_TAG) material.color = Color.black;
+        if(tile.tag == GROUND_TAG) material.color = Color.white;
         tileRenderer.material = material;
         return material;
     }
     GameObject GenerateWallTile(int x, int z)
     {
         GameObject wall = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        wall.name = "Wall("+x+","+z+")";
         wall.transform.position = new Vector3(x,0.25f,z);
         wall.transform.localScale = new Vector3(1,1.5f,1);
-        wall.tag = "Wall";
+        wall.tag = WALL_TAG;
+        tiles[x,z] = wall;
         return wall;
     }
 
     GameObject GenerateGroundTile(int x, int z)
     {
         GameObject ground = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        ground.name = "Tile("+x+","+z+")";
         ground.transform.position = new Vector3(x,0,z);
-        ground.tag = "Ground";
+        ground.tag = GROUND_TAG;
+        tiles[x,z] = ground;
         return ground;
+    }
+
+    private List<Vector3> directions = new List<Vector3>{Vector3.forward, Vector3.back, Vector3.right, Vector3.left};
+    public List<GameObject> BFS(GameObject root, int maxRange)
+    {
+        List<GameObject> selectableTile = new List<GameObject>();
+        Queue<GameObject> tilesToBeVisited = new Queue<GameObject>();
+        Queue<int> parentRangeQueue = new Queue<int>();
+        bool[,] visitedTiles = new bool[xSize, zSize];
+        for(int i = 0; i < xSize; i++)
+            for(int j = 0; j < zSize; j++)
+                visitedTiles[i,j] = false;
+
+        tilesToBeVisited.Enqueue(root);
+        parentRangeQueue.Enqueue(0);
+        visitedTiles[(int)root.transform.position.x, (int)root.transform.position.z] = true;
+
+        while (tilesToBeVisited.Count > 0)
+        {
+            var tile =  tilesToBeVisited.Dequeue();
+            var range = parentRangeQueue.Dequeue();
+            if(range > maxRange) continue;
+            for (int i = 0; i < 4; i++)
+            {
+                int x = (int) (tile.transform.position.x + directions[i].x);
+                int z = (int) (tile.transform.position.z + directions[i].z);
+                int newRange = range+1;
+                if(newRange <= maxRange && isValid(x,z) && !visitedTiles[x,z]&& tiles[x,z].tag == GROUND_TAG)
+                {
+                    GameObject newTile = tiles[x,z];
+                    if(newTile == null) continue;
+                    selectableTile.Add(newTile);
+                    tilesToBeVisited.Enqueue(newTile);
+                    parentRangeQueue.Enqueue(newRange);
+                    visitedTiles[x,z] = true;
+                }  
+            }            
+        }
+        return selectableTile;
+    }
+
+    public Stack<GameObject> AStarSearch(GameObject start, GameObject dest)
+    {
+        if(!isValidForAStarSearch(start, dest)) return null;
+
+        bool[,] closedList = new bool[xSize, zSize];
+        Cell[,] cellsDetails = new Cell[xSize,zSize];
+        int i, j;
+        InitAStarSearch(ref closedList, ref cellsDetails);   
+
+        i = (int)start.transform.position.x;
+        j = (int)start.transform.position.z;
+        ChangeCellValue(ref cellsDetails[i,j], i, j, 0,0,0);
+
+        Queue<(float f,(int x, int z) pos)> openList = new Queue<(float f, (int x, int z) pos)>();
+        openList.Enqueue((0, (i,j)));
+
+        while(openList.Count > 0)
+        {
+            var p = openList.Dequeue();
+            i = p.pos.x;
+            j = p.pos.z;
+            closedList[i,j] = true;
+
+            float fNew, gNew, hNew;
+            foreach (Vector3 direction in directions)
+            {
+                int iNew = (int)(i+direction.x);
+                int jNew = (int)(j+direction.z);
+                if(isValid(iNew,jNew))
+                {
+                    if(tiles[iNew,jNew] == dest)
+                    {
+                        cellsDetails[iNew, jNew].i = i;
+                        cellsDetails[iNew, jNew].j = j;
+                        return TracePath(cellsDetails, dest);
+                    }
+                    else if(closedList[iNew,jNew] == false && IsUnblocked(tiles[iNew, jNew]))
+                    {
+                        gNew = cellsDetails[i, j].g + 1;
+                        hNew = (float)CalculateHValue(iNew, jNew, ((int)dest.transform.position.x, (int)dest.transform.position.z));
+                        fNew = gNew + hNew;
+                        if(cellsDetails[iNew, jNew].f == Mathf.Infinity || cellsDetails[iNew, jNew].f > fNew)
+                        {
+                            openList.Enqueue((fNew,(iNew, jNew)));
+                            ChangeCellValue(ref cellsDetails[iNew, jNew], i, j, fNew, gNew, hNew);
+                        }
+                    }
+                }
+            }
+        }
+        return new Stack<GameObject>();
+    }
+
+    private bool IsUnblocked(GameObject tile)
+    {
+        return tile != null && tile.tag == GROUND_TAG;
+    }
+
+    private void InitAStarSearch(ref bool[,] closedList, ref Cell[,] cellsDetails)
+    {
+        for (int i = 0; i < xSize; i++)
+        {
+            for (int j = 0; j < zSize; j++)
+            {
+                closedList[i,j] = false;
+                ChangeCellValue(ref cellsDetails[i,j], -1, -1, Mathf.Infinity, Mathf.Infinity, Mathf.Infinity);
+            }
+                
+        } 
+    }
+
+    private bool isValidForAStarSearch(GameObject start, GameObject dest)
+    {
+        return isValid((int)start.transform.position.x, (int)start.transform.position.z) && isValid((int)dest.transform.position.x, (int)dest.transform.position.z)
+            && start.tag == GROUND_TAG && dest.tag == GROUND_TAG
+            && start != dest;
+    }
+
+    private void ChangeCellValue(ref Cell cell, int i, int j, float f, float g, float h)
+    {
+        cell.i = i;
+        cell.j = j;
+        cell.f = f;
+        cell.g = g;
+        cell.h = h;
+    }
+
+    struct Cell
+    {
+        public int i,j;
+        public float f,g,h;
+    }
+
+    private bool isValid(int x, int z)
+    {
+        return x>=0 && x<xSize && z>=0 && z<zSize;
+    }
+
+    private Stack<GameObject> TracePath(Cell[,] cells, GameObject dest)
+    {
+        int x = (int)dest.transform.position.x;
+        int z = (int)dest.transform.position.z;
+        Stack<GameObject> path = new Stack<GameObject>();
+        while (!(cells[x,z].i == x && cells[x,z].j == z))
+        {
+           path.Push(tiles[x,z]);
+           int temp_x = cells[x,z].i;
+           int temp_z = cells[x,z].j;
+           x = temp_x;
+           z = temp_z;
+        }
+        return path;
+    }
+
+    private int CalculateHValue(int x, int z, (int x, int z) dest)
+    {
+        return Mathf.Abs(x-dest.x)+Mathf.Abs(z-dest.z);
     }
 }
---
+++ MVP/client/MVP/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int movementPoint = 5;
+    public float moveSpeed = 2;
+    public GameObject currentTile;
+    private List<GameObject> selectableTiles;
+    private Stack<GameObject> path;
+    private bool isMoving;
+
+    private readonly Color NORMAL_COLOR = Color.white;
+    private readonly Color SELECTABLE_TILE_COLOR = Color.blue;
+    private readonly Color PATH_TILE_COLOR = Color.green;
+
+    
+    private void Update() {
+        if(selectableTiles != null)
+            TileSelection();
+        if(path!=null)
+            MoveTo();
+    }
+
+    public GameObject GetCurrentTile()
+    {
+        return GameObject.FindGameObjectsWithTag(MapGenerator.GROUND_TAG).First(tile => tile.transform.position.x == transform.position.x && tile.transform.position.z == transform.position.z);
+    }
+
+    public void GetSelectableTiles()
+    {
+        if(path != null && path.Count>0) return;
+        currentTile = GetCurrentTile();
+        selectableTiles = GameObject.Find("MapGenerator").GetComponent<MapGenerator>().BFS(currentTile, movementPoint);
+        HighlightTiles(selectableTiles, SELECTABLE_TILE_COLOR);
+    }
+
+    void HighlightPathTiles(Stack<GameObject> path)
+    {
+        if(path == null || path.Count <= 0) return;
+        HighlightTiles(path, PATH_TILE_COLOR);
+    }
+
+    void HighlightTiles(IEnumerable<GameObject> tiles, Color color)
+    {
+        foreach (GameObject tile in tiles)
+            tile.GetComponent<Renderer>().material.color = color;
+    }
+
+    private void MoveTo()
+    {
+        if(path == null || path.Count <= 0) return;
+        GameObject tile = path.Peek();
+        Vector3 target = tile.transform.position;
+        target.y += GetComponent<Collider>().bounds.extents.y + tile.GetComponent<Collider>().bounds.extents.y;
+        if(Vector3.Distance(transform.position, target) >= 0.05f)
+        {
+            Vector3 heading = target-transform.position;
+            heading.Normalize();
+            Vector3 velocity = heading*moveSpeed;
+            transform.forward = heading;
+            transform.position += velocity * Time.deltaTime;
+        }else
+        {   
+            transform.position = target;
+            path.Pop();
+        }
+    }
+
+    private void TileSelection()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit))
+            if(IsSelectedTileValidForMovement(hit.collider.gameObject))
+                FindPath(hit.collider.gameObject);
+    }
+
+    void FindPath(GameObject target){
+        Stack<GameObject> tempPath = GameObject.Find("MapGenerator").GetComponent<MapGenerator>().AStarSearch(currentTile, target);
+        HighlightTiles(selectableTiles, SELECTABLE_TILE_COLOR);
+        HighlightPathTiles(tempPath);
+        if(Input.GetMouseButtonUp(0))
+            SetPath(tempPath);
+    }
+    void SetPath(Stack<GameObject> tempPath)
+    {
+        path = tempPath;
+        HighlightTiles(selectableTiles, NORMAL_COLOR);
+        selectableTiles = null;
+    }
+
+    bool IsSelectedTileValidForMovement(GameObject tile)
+    {
+        return selectableTiles != null && tile.tag == MapGenerator.GROUND_TAG && selectableTiles.Contains(tile);
+    }
+    
+}
