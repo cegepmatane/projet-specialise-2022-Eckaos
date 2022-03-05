af50582def3ba0755c916409bec37144c8cd337f
Eckaos <nathan.pollart@hotmail.fr>
2022-03-05 16:39:39 -0500

    Refactoring

--- MVP/client/MVP/Assets/Scripts/Map/MapGenerator.cs
+++ MVP/client/MVP/Assets/Scripts/Map/MapGenerator.cs
@@ -6,7 +6,7 @@ public class MapGenerator : MonoBehaviour
     // Start is called before the first frame update
     private bool[,] walls;
     public Material tileMaterial;
-
+    private TileMap tileMap;
     public int xSize = 5;
     public int zSize = 5;
     private int numberOfWalls;
@@ -76,7 +76,7 @@ public class MapGenerator : MonoBehaviour
     {
         GameObject map = new GameObject(MAP_TAG);
         map.tag = MAP_TAG;
-        tiles = new GameObject[xSize,zSize];
+        tileMap = TileMap.GetInstance(xSize, zSize);
         for (int x = 0; x < xSize; x++)
         {
             GameObject row = new GameObject("Row"+x);
@@ -92,6 +92,7 @@ public class MapGenerator : MonoBehaviour
                 tile.transform.SetParent(row.transform);
                 Renderer tileRenderer = tile.GetComponent<Renderer>();
                 tileRenderer.material = GenerateMaterial(tile);
+                tileMap.SetTile(x,z, tile);
             }
         }
     }
@@ -113,7 +114,6 @@ public class MapGenerator : MonoBehaviour
         wall.transform.position = new Vector3(x,0.25f,z);
         wall.transform.localScale = new Vector3(1,1.5f,1);
         wall.tag = WALL_TAG;
-        tiles[x,z] = wall;
         return wall;
     }
 
@@ -123,165 +123,6 @@ public class MapGenerator : MonoBehaviour
         ground.name = "Tile("+x+","+z+")";
         ground.transform.position = new Vector3(x,0,z);
         ground.tag = GROUND_TAG;
-        tiles[x,z] = ground;
         return ground;
-    }
-
-    private List<Vector3> directions = new List<Vector3>{Vector3.forward, Vector3.back, Vector3.right, Vector3.left};
-    public List<GameObject> BFS(GameObject root, int maxRange)
-    {
-        List<GameObject> selectableTile = new List<GameObject>();
-        Queue<GameObject> tilesToBeVisited = new Queue<GameObject>();
-        Queue<int> parentRangeQueue = new Queue<int>();
-        bool[,] visitedTiles = new bool[xSize, zSize];
-        for(int i = 0; i < xSize; i++)
-            for(int j = 0; j < zSize; j++)
-                visitedTiles[i,j] = false;
-
-        tilesToBeVisited.Enqueue(root);
-        parentRangeQueue.Enqueue(0);
-        visitedTiles[(int)root.transform.position.x, (int)root.transform.position.z] = true;
-
-        while (tilesToBeVisited.Count > 0)
-        {
-            var tile =  tilesToBeVisited.Dequeue();
-            var range = parentRangeQueue.Dequeue();
-            if(range > maxRange) continue;
-            for (int i = 0; i < 4; i++)
-            {
-                int x = (int) (tile.transform.position.x + directions[i].x);
-                int z = (int) (tile.transform.position.z + directions[i].z);
-                int newRange = range+1;
-                if(newRange <= maxRange && isValid(x,z) && !visitedTiles[x,z]&& tiles[x,z].tag == GROUND_TAG)
-                {
-                    GameObject newTile = tiles[x,z];
-                    if(newTile == null) continue;
-                    selectableTile.Add(newTile);
-                    tilesToBeVisited.Enqueue(newTile);
-                    parentRangeQueue.Enqueue(newRange);
-                    visitedTiles[x,z] = true;
-                }  
-            }            
-        }
-        return selectableTile;
-    }
-
-    public Stack<GameObject> AStarSearch(GameObject start, GameObject dest)
-    {
-        if(!isValidForAStarSearch(start, dest)) return null;
-
-        bool[,] closedList = new bool[xSize, zSize];
-        Cell[,] cellsDetails = new Cell[xSize,zSize];
-        int i, j;
-        InitAStarSearch(ref closedList, ref cellsDetails);   
-
-        i = (int)start.transform.position.x;
-        j = (int)start.transform.position.z;
-        ChangeCellValue(ref cellsDetails[i,j], i, j, 0,0,0);
-
-        Queue<(float f,(int x, int z) pos)> openList = new Queue<(float f, (int x, int z) pos)>();
-        openList.Enqueue((0, (i,j)));
-
-        while(openList.Count > 0)
-        {
-            var p = openList.Dequeue();
-            i = p.pos.x;
-            j = p.pos.z;
-            closedList[i,j] = true;
-
-            float fNew, gNew, hNew;
-            foreach (Vector3 direction in directions)
-            {
-                int iNew = (int)(i+direction.x);
-                int jNew = (int)(j+direction.z);
-                if(isValid(iNew,jNew))
-                {
-                    if(tiles[iNew,jNew] == dest)
-                    {
-                        cellsDetails[iNew, jNew].i = i;
-                        cellsDetails[iNew, jNew].j = j;
-                        return TracePath(cellsDetails, dest);
-                    }
-                    else if(closedList[iNew,jNew] == false && IsUnblocked(tiles[iNew, jNew]))
-                    {
-                        gNew = cellsDetails[i, j].g + 1;
-                        hNew = (float)CalculateHValue(iNew, jNew, ((int)dest.transform.position.x, (int)dest.transform.position.z));
-                        fNew = gNew + hNew;
-                        if(cellsDetails[iNew, jNew].f == Mathf.Infinity || cellsDetails[iNew, jNew].f > fNew)
-                        {
-                            openList.Enqueue((fNew,(iNew, jNew)));
-                            ChangeCellValue(ref cellsDetails[iNew, jNew], i, j, fNew, gNew, hNew);
-                        }
-                    }
-                }
-            }
-        }
-        return new Stack<GameObject>();
-    }
-
-    private bool IsUnblocked(GameObject tile)
-    {
-        return tile != null && tile.tag == GROUND_TAG;
-    }
-
-    private void InitAStarSearch(ref bool[,] closedList, ref Cell[,] cellsDetails)
-    {
-        for (int i = 0; i < xSize; i++)
-        {
-            for (int j = 0; j < zSize; j++)
-            {
-                closedList[i,j] = false;
-                ChangeCellValue(ref cellsDetails[i,j], -1, -1, Mathf.Infinity, Mathf.Infinity, Mathf.Infinity);
-            }
-                
-        } 
-    }
-
-    private bool isValidForAStarSearch(GameObject start, GameObject dest)
-    {
-        return isValid((int)start.transform.position.x, (int)start.transform.position.z) && isValid((int)dest.transform.position.x, (int)dest.transform.position.z)
-            && start.tag == GROUND_TAG && dest.tag == GROUND_TAG
-            && start != dest;
-    }
-
-    private void ChangeCellValue(ref Cell cell, int i, int j, float f, float g, float h)
-    {
-        cell.i = i;
-        cell.j = j;
-        cell.f = f;
-        cell.g = g;
-        cell.h = h;
-    }
-
-    struct Cell
-    {
-        public int i,j;
-        public float f,g,h;
-    }
-
-    private bool isValid(int x, int z)
-    {
-        return x>=0 && x<xSize && z>=0 && z<zSize;
-    }
-
-    private Stack<GameObject> TracePath(Cell[,] cells, GameObject dest)
-    {
-        int x = (int)dest.transform.position.x;
-        int z = (int)dest.transform.position.z;
-        Stack<GameObject> path = new Stack<GameObject>();
-        while (!(cells[x,z].i == x && cells[x,z].j == z))
-        {
-           path.Push(tiles[x,z]);
-           int temp_x = cells[x,z].i;
-           int temp_z = cells[x,z].j;
-           x = temp_x;
-           z = temp_z;
-        }
-        return path;
-    }
-
-    private int CalculateHValue(int x, int z, (int x, int z) dest)
-    {
-        return Mathf.Abs(x-dest.x)+Mathf.Abs(z-dest.z);
     }
 }
---
+++ MVP/client/MVP/Assets/Scripts/Map/TileMap.cs
@@ -0,0 +1,237 @@
+using System.Collections.Generic;
+using UnityEngine;
+public class TileMap {
+    
+    private static TileMap map;
+    
+    private Tile[,] tileMap;
+    private int xSize = 10;
+    private int zSize = 10;
+    private List<Vector3> directions = new List<Vector3>{Vector3.forward, Vector3.back, Vector3.right, Vector3.left};
+
+    private TileMap(int xSize, int zSize)
+    {
+        tileMap = new Tile[xSize, zSize];
+        this.xSize = xSize;
+        this.zSize = zSize;
+    }
+
+    public static TileMap GetInstance(int xSize = 10, int zSize = 10)
+    {
+        if(map == null) map = new TileMap(xSize, zSize);
+        return map;
+    }
+
+    public Tile GetTile(int x, int z){
+        if(!IsValid(x, z)) return null;
+        return tileMap[x,z];
+    } 
+    public void SetTile(int x, int z, GameObject ground = null, GameObject player = null) => tileMap[x,z] = new Tile(x,z, ground, player);
+
+    public List<Tile> MovingBFS(Tile root, int maxRange)
+    {
+        List<Tile> selectableTiles = new List<Tile>();
+        if(root == null) return selectableTiles;
+
+        Queue<Tile> tileToBeVisited = new Queue<Tile>();
+        Queue<int> parentRangeQueue = new Queue<int>();
+        bool[,] visitedTiles = new bool[xSize, zSize];
+
+        for(int i = 0; i < xSize; i++)
+            for(int j = 0; j < zSize; j++)
+                visitedTiles[i,j] = false;
+
+        tileToBeVisited.Enqueue(root);
+        parentRangeQueue.Enqueue(0);
+        visitedTiles[root.x, root.z] = true;
+
+        while (tileToBeVisited.Count > 0)
+        {
+            var tile =  tileToBeVisited.Dequeue();
+            var range = parentRangeQueue.Dequeue();
+            if(range > maxRange) continue;
+            for (int i = 0; i < 4; i++)
+            {
+                int x = (int) (tile.x + directions[i].x);
+                int z = (int) (tile.z + directions[i].z);
+                int newRange = range+1;
+                if(newRange <= maxRange && IsValid(x,z) && !visitedTiles[x,z] && tileMap[x,z].IsWalkable())
+                {
+                    Tile newTile = tileMap[x,z];
+                    selectableTiles.Add(newTile);
+                    tileToBeVisited.Enqueue(newTile);
+                    parentRangeQueue.Enqueue(newRange);
+                    visitedTiles[x,z] = true;
+                }  
+            }            
+        }
+        return selectableTiles;
+    }
+
+    public List<Tile> AttackBFS(Tile root, int maxRange)
+    {
+        List<Tile> selectableTiles = new List<Tile>();
+        if(root == null) return selectableTiles;
+
+        Queue<Tile> tileToBeVisited = new Queue<Tile>();
+        Queue<int> parentRangeQueue = new Queue<int>();
+        bool[,] visitedTiles = new bool[xSize, zSize];
+
+        for(int i = 0; i < xSize; i++)
+            for(int j = 0; j < zSize; j++)
+                visitedTiles[i,j] = false;
+
+        tileToBeVisited.Enqueue(root);
+        parentRangeQueue.Enqueue(0);
+        visitedTiles[root.x, root.z] = true;
+
+        while (tileToBeVisited.Count > 0)
+        {
+            var tile =  tileToBeVisited.Dequeue();
+            var range = parentRangeQueue.Dequeue();
+            if(range > maxRange) continue;
+            for (int i = 0; i < 4; i++)
+            {
+                int x = (int) (tile.x + directions[i].x);
+                int z = (int) (tile.z + directions[i].z);
+                int newRange = range+1;
+                if(newRange <= maxRange && IsValid(x,z) && !visitedTiles[x,z] && tileMap[x,z].IsGround())
+                {
+                    Tile newTile = tileMap[x,z];
+                    selectableTiles.Add(newTile);
+                    tileToBeVisited.Enqueue(newTile);
+                    parentRangeQueue.Enqueue(newRange);
+                    visitedTiles[x,z] = true;
+                }  
+            }            
+        }
+        return selectableTiles;
+    }
+
+    private bool IsValid(int x, int z)
+    {
+        return x>=0 && x<xSize && z>=0 && z<zSize;
+    }
+
+
+    public Stack<Tile> AStarSearch(Tile start, Tile dest)
+    {
+        if(!isValidForAStarSearch(start, dest)) return null;
+
+        bool[,] closedList = new bool[xSize, zSize];
+        Cell[,] cellsDetails = new Cell[xSize,zSize];
+        int i, j;
+        InitAStarSearch(ref closedList, ref cellsDetails);   
+
+        i = (int)start.x;
+        j = (int)start.z;
+        ChangeCellValue(ref cellsDetails[i,j], i, j, 0,0,0);
+
+        Queue<(float f,(int x, int z) pos)> openList = new Queue<(float f, (int x, int z) pos)>();
+        openList.Enqueue((0, (i,j)));
+
+        while(openList.Count > 0)
+        {
+            var p = openList.Dequeue();
+            i = p.pos.x;
+            j = p.pos.z;
+            closedList[i,j] = true;
+
+            float fNew, gNew, hNew;
+            foreach (Vector3 direction in directions)
+            {
+                int iNew = (int)(i+direction.x);
+                int jNew = (int)(j+direction.z);
+                if(IsValid(iNew,jNew))
+                {
+                    if(tileMap[iNew,jNew] == dest)
+                    {
+                        cellsDetails[iNew, jNew].i = i;
+                        cellsDetails[iNew, jNew].j = j;
+                        return TracePath(cellsDetails, dest);
+                    }
+                    else if(closedList[iNew,jNew] == false && tileMap[iNew, jNew].IsWalkable())
+                    {
+                        gNew = cellsDetails[i, j].g + 1;
+                        hNew = (float)CalculateHValue(iNew, jNew, (dest.x, dest.z));
+                        fNew = gNew + hNew;
+                        if(cellsDetails[iNew, jNew].f == Mathf.Infinity || cellsDetails[iNew, jNew].f > fNew)
+                        {
+                            openList.Enqueue((fNew,(iNew, jNew)));
+                            ChangeCellValue(ref cellsDetails[iNew, jNew], i, j, fNew, gNew, hNew);
+                        }
+                    }
+                }
+            }
+        }
+        return new Stack<Tile>();
+    }
+    struct Cell
+    {
+        public int i,j;
+        public float f,g,h;
+    }
+
+    private bool isValidForAStarSearch(Tile start, Tile dest) => IsValid(start.x, start.z) && IsValid(dest.x, dest.z) && dest.IsWalkable() && start != dest;
+    private void InitAStarSearch(ref bool[,] closedList, ref Cell[,] cellsDetails)
+    {
+        for (int i = 0; i < xSize; i++)
+        {
+            for (int j = 0; j < zSize; j++)
+            {
+                closedList[i,j] = false;
+                ChangeCellValue(ref cellsDetails[i,j], -1, -1, Mathf.Infinity, Mathf.Infinity, Mathf.Infinity);
+            }
+                
+        } 
+    }
+
+    private void ChangeCellValue(ref Cell cell, int i, int j, float f, float g, float h)
+    {
+        cell.i = i;
+        cell.j = j;
+        cell.f = f;
+        cell.g = g;
+        cell.h = h;
+    }
+
+    private Stack<Tile> TracePath(Cell[,] cells, Tile dest)
+    {
+        int x = (int)dest.x;
+        int z = (int)dest.z;
+        Stack<Tile> path = new Stack<Tile>();
+        while (!(cells[x,z].i == x && cells[x,z].j == z))
+        {
+           path.Push(tileMap[x,z]);
+           int temp_x = cells[x,z].i;
+           int temp_z = cells[x,z].j;
+           x = temp_x;
+           z = temp_z;
+        }
+        return path;
+    }
+
+    private int CalculateHValue(int x, int z, (int x, int z) dest) => Mathf.Abs(x-dest.x)+Mathf.Abs(z-dest.z);
+}
+
+public class Tile
+{
+    public GameObject ground;
+    public GameObject player;
+    public int x;
+    public int z;
+
+    public Tile(int x, int z, GameObject ground, GameObject player)
+    {
+        this.ground = ground;
+        this.player = player;
+        this.x = x;
+        this.z = z;
+    }
+
+    public bool IsWalkable() => IsGround() && player == null;
+    public bool IsGround() => ground.tag == "Ground";
+    
+    public static bool operator ==(Tile tile, Tile tile2) => !(tile is null) && !(tile2 is null) && tile.x == tile2.x && tile.z == tile2.z && tile.ground == tile2.ground && tile.player == tile2.player;
+    public static bool operator !=(Tile tile, Tile tile2) => tile is null || tile2 is null || tile.x != tile2.x || tile.z != tile2.z || tile.ground != tile2.ground || tile.player != tile2.player;
+}
