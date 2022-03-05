40dbef157bcb5beda52b39ce3db917ed36bca7c9
Eckaos <nathan.pollart@hotmail.fr>
2022-03-05 16:42:04 -0500

    Refactoring

--- MVP/client/MVP/Assets/Scripts/Player/PlayerMovement.cs
+++ MVP/client/MVP/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,54 +7,57 @@ public class PlayerMovement : MonoBehaviour
 {
     public int movementPoint = 5;
     public float moveSpeed = 2;
-    public GameObject currentTile;
-    private List<GameObject> selectableTiles;
-    private Stack<GameObject> path;
+    private List<Tile> selectableTiles;
+    private Stack<Tile> path;
     private bool isMoving;
+
+    private TileMap map;
+    private Tile currentTile;
 
     private readonly Color NORMAL_COLOR = Color.white;
     private readonly Color SELECTABLE_TILE_COLOR = Color.blue;
     private readonly Color PATH_TILE_COLOR = Color.green;
 
-    
     private void Update() {
+        GetCurrentTile();
         if(selectableTiles != null)
             TileSelection();
         if(path!=null)
             MoveTo();
     }
 
-    public GameObject GetCurrentTile()
+    private Tile GetCurrentTile()
     {
-        return GameObject.FindGameObjectsWithTag(MapGenerator.GROUND_TAG).First(tile => tile.transform.position.x == transform.position.x && tile.transform.position.z == transform.position.z);
+        currentTile = TileMap.GetInstance().GetTile((int)transform.position.x, (int)transform.position.z);
+        currentTile.player = gameObject;
+        return currentTile;
     }
 
     public void GetSelectableTiles()
     {
         if(path != null && path.Count>0) return;
-        currentTile = GetCurrentTile();
-        selectableTiles = GameObject.Find("MapGenerator").GetComponent<MapGenerator>().BFS(currentTile, movementPoint);
+        selectableTiles = TileMap.GetInstance().MovingBFS(GetCurrentTile(), movementPoint);
         HighlightTiles(selectableTiles, SELECTABLE_TILE_COLOR);
     }
 
-    void HighlightPathTiles(Stack<GameObject> path)
+    void HighlightPathTiles(Stack<Tile> path)
     {
         if(path == null || path.Count <= 0) return;
         HighlightTiles(path, PATH_TILE_COLOR);
     }
 
-    void HighlightTiles(IEnumerable<GameObject> tiles, Color color)
+    void HighlightTiles(IEnumerable<Tile> tiles, Color color)
     {
-        foreach (GameObject tile in tiles)
-            tile.GetComponent<Renderer>().material.color = color;
+        foreach (Tile tile in tiles)
+            tile.ground.GetComponent<Renderer>().material.color = color;
     }
 
     private void MoveTo()
     {
         if(path == null || path.Count <= 0) return;
-        GameObject tile = path.Peek();
-        Vector3 target = tile.transform.position;
-        target.y += GetComponent<Collider>().bounds.extents.y + tile.GetComponent<Collider>().bounds.extents.y;
+        Tile tile = path.Peek();
+        Vector3 target = tile.ground.transform.position;
+        target.y += GetComponent<Collider>().bounds.extents.y + tile.ground.GetComponent<Collider>().bounds.extents.y;
         if(Vector3.Distance(transform.position, target) >= 0.05f)
         {
             Vector3 heading = target-transform.position;
@@ -65,6 +68,8 @@ public class PlayerMovement : MonoBehaviour
         }else
         {   
             transform.position = target;
+            currentTile.player = null; 
+            currentTile = GetCurrentTile();
             path.Pop();
         }
     }
@@ -74,29 +79,34 @@ public class PlayerMovement : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         if(Physics.Raycast(ray, out hit))
-            if(IsSelectedTileValidForMovement(hit.collider.gameObject))
-                FindPath(hit.collider.gameObject);
+        {
+            Vector3 targetPosition = hit.collider.transform.position;
+            Tile target = TileMap.GetInstance().GetTile((int)targetPosition.x ,(int)targetPosition.z);
+            if(IsSelectedTileValidForMovement(target))
+                FindPath(target);
             else
                 HighlightTiles(selectableTiles, SELECTABLE_TILE_COLOR);
+        }
+            
     }
 
-    void FindPath(GameObject target){
-        Stack<GameObject> tempPath = GameObject.Find("MapGenerator").GetComponent<MapGenerator>().AStarSearch(currentTile, target);
+    void FindPath(Tile target){
+        Stack<Tile> tempPath = TileMap.GetInstance().AStarSearch(GetCurrentTile(), target);
         HighlightTiles(selectableTiles, SELECTABLE_TILE_COLOR);
         HighlightPathTiles(tempPath);
         if(Input.GetMouseButtonUp(0))
             SetPath(tempPath);
     }
-    void SetPath(Stack<GameObject> tempPath)
+    void SetPath(Stack<Tile> tempPath)
     {
         path = tempPath;
         HighlightTiles(selectableTiles, NORMAL_COLOR);
         selectableTiles = null;
     }
 
-    bool IsSelectedTileValidForMovement(GameObject tile)
+    bool IsSelectedTileValidForMovement(Tile tile)
     {
-        return selectableTiles != null && tile.tag == MapGenerator.GROUND_TAG && selectableTiles.Contains(tile);
+        return selectableTiles != null && tile.IsWalkable() && selectableTiles.Contains(tile);
     }
     
 }
