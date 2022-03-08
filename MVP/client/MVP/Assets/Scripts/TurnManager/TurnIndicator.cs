24814cf886e1f451469aa3955f30771bbf0d6bef
Eckaos <nathan.pollart@hotmail.fr>
2022-03-07 22:11:29 -0500

    UI Turn Indicator presque fini #5 + amélioration de l'UI global
    couleur pas encore set

--- MVP/client/MVP/Assets/Scripts/TurnManager/TurnIndicator.cs
+++ MVP/client/MVP/Assets/Scripts/TurnManager/TurnIndicator.cs
@@ -2,34 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class TurnIndicator : MonoBehaviour
 {
     private List<Image> indicators;
-    private float offset = 45;
+    private float offset = -70;
     private int nbOffset = 0;
 
+    private Vector2 indicatorSize = new Vector2(60, 90);
+    public RectTransform turnIndicatorPosition;
+
+    private void Start() {
+        
+    }
 
     public void UpdateIndicator(List<Character> characters, Character character)
     {
+        if(indicators != null)
+            foreach (Image img in indicators)
+                DestroyImmediate(img.gameObject);
         indicators = new List<Image>();
         nbOffset = 0;
-        foreach (Character c in characters)
-            indicators.Add(CreateImage());
+        for (int i = characters.Count - 1; i >= 0 ; i--)
+        {
+            Image img = CreateCharacterIndicator();
+            if(characters[i]==character) img.color = Color.cyan;
+            indicators.Add(img);
+        }
+
+        turnIndicatorPosition.sizeDelta = new Vector2(indicators.Count * indicatorSize.x + (indicators.Count+1) * 10 ,turnIndicatorPosition.sizeDelta.y);
+        turnIndicatorPosition.ForceUpdateRectTransforms();
+    }
+
+    private Image CreateCharacterIndicator()
+    {   
+        Image image = CreateImage();
+        RectTransform pos = image.GetComponent<RectTransform>();
+        pos.SetParent(turnIndicatorPosition);
+        SetUpImage(pos);
+        pos.ForceUpdateRectTransforms();
+        nbOffset++;
+        return image;
     }
 
     private Image CreateImage()
     {
-        GameObject obj = new GameObject("test");
+        GameObject obj = new GameObject("Indicator");
         Image img = obj.AddComponent<Image>();
-        RectTransform pos = obj.GetComponent<RectTransform>();
-        pos.SetParent(transform);
-        pos.localScale = new Vector3(0.4f, 0.5f, 1);
-        pos.localPosition = new Vector3(120+offset*nbOffset,-120,0);
-        pos.ForceUpdateRectTransforms();
-        obj.SetActive(true);
-        nbOffset++;
         return img;
+    }
+    
+    private void SetUpImage(RectTransform pos)
+    {
+        pos.sizeDelta = indicatorSize;
+        pos.localScale = new Vector3(1,1,1);
+        pos.localPosition = new Vector3(-40+nbOffset*offset,0,0);
+        pos.anchorMin = new Vector2(1, 0.5f);
+        pos.anchorMax = new Vector2(1, 0.5f);
+        pos.pivot = new Vector2(0.5f, 0.5f);
     }
     
 }
